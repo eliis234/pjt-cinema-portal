d8aba44855fdaaa6d3198f6fefa807ad49a8dc1f
Duy Thai <nguyenduythai427@gmail.com>
2019-11-21 19:39:45 +0700

    dfghjkl;

--- layout-v2/src/app/admin/admin-page/history-management/history-management.component.ts
+++ layout-v2/src/app/admin/admin-page/history-management/history-management.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 interface HistoryFilm {
   // historyId: string;
@@ -8,10 +12,42 @@ interface HistoryFilm {
   // filmId: string;
   filmName: string;
   amount: number;
-  price: string;
+  price: number;
 }
 
-const DATA:HistoryFilm[] = []
+const DATA: HistoryFilm[] = [
+  {
+    userName: 'Ben Nguyen',
+    time: '20/11/2019 08:00',
+    filmName: 'Ted 35',
+    amount: 2,
+    price: 570000
+  }, {
+    userName: 'Ben Nguyen',
+    time: '20/11/2019 08:05',
+    filmName: 'Trainwreck',
+    amount: 1,
+    price: 580000
+  }, {
+    userName: 'Mai Hoang Minh',
+    time: '20/11/2019 10:00',
+    filmName: 'Ted 35',
+    amount: 2,
+    price: 570000
+  }, {
+    userName: 'Mai Hoang Minh',
+    time: '20/11/2019 12:15',
+    filmName: 'Home',
+    amount: 1,
+    price: 580000
+  }, {
+    userName: 'Nguyen Hoang Nhan',
+    time: '20/11/2019 13:20',
+    filmName: 'Ant-Man',
+    amount: 3,
+    price: 550000
+  }
+]
 
 @Component({
   selector: 'app-history-management',
@@ -19,12 +55,27 @@ const DATA:HistoryFilm[] = []
   styleUrls: ['./history-management.component.scss']
 })
 export class HistoryManagementComponent implements OnInit {
+  displayedColumns: string[] = ['index', 'userName', 'time', 'filmName', 'amount', 'price', 'sum'];
+  dataSource: MatTableDataSource<HistoryFilm>;
 
-  constructor() { }
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  ngOnInit() {
+  constructor() {
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(DATA);
   }
 
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
-
-
