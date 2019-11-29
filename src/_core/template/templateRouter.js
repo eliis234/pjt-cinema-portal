ea36e328b0a9f276d21cdb6412c0deb78136bd52
bennguyen232 <nguyenduythai427@gmail.com>
2019-11-29 20:20:07 +0700

    abasbd

--- src/_core/template/templateRouter.js
+++ src/_core/template/templateRouter.js
@@ -3,7 +3,7 @@ import Header from "../component/Header";
 
 export class HomeTemplate extends React.Component {
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     const Cmp = this.props.Component
     return (
       <>
