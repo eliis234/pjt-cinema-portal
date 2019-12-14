dca8b492abddb9e4f211572a7133c52391d8284b
bennguyen232 <nguyenduythai427@gmail.com>
2019-12-14 17:16:52 +0700

    fuck

--- src/component/v2/home/CarouselCpm.js
+++ src/component/v2/home/CarouselCpm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import OwlCarousel from "react-owl-carousel2";
-import './styles/carousel.scss';
+import "./styles/carousel.scss";
 
 export default class CarouselCpm extends Component {
   render() {
@@ -8,7 +8,11 @@ export default class CarouselCpm extends Component {
       items: 1,
       nav: true,
       rewind: true,
-      autoplay: true
+      autoplay: true,
+      navText: [
+        '<i class="fas fa-chevron-left"></i>',
+        '<i class="fas fa-chevron-right"></i>'
+      ],
     };
 
     const events = {
@@ -16,18 +20,52 @@ export default class CarouselCpm extends Component {
       onChanged: function(event) {}
     };
     return (
-      <section className="carousel-cpm">
-        <OwlCarousel ref="car" options={options} events={events}>
-          <div>
-            <img src="/img/images/carousel/carousel1.jpg" alt="The Last" />
+      <section className="carousel-cpm ">
+        <div className="carousel-booking">
+          <OwlCarousel ref="car" options={options} events={events}>
+            <div>
+              <img src="/img/images/carousel/carousel1.jpg" alt="The Last" />
+            </div>
+            <div>
+              <img src="/img/images/carousel/carousel2.jpg" alt="GTA V" />
+            </div>
+            <div>
+              <img src="/img/images/carousel/carousel3.jpg" alt="Mirror Edge" />
+            </div>
+          </OwlCarousel>
+
+          <div className="booking-ticket">
+            <div className="row m-0">
+              <div className="col-sm-6 col-md-4">
+                <select id="selectMovie" className="form-control">
+                  <option selected>Phim</option>
+                  <option>item.TenPhim</option>
+                </select>
+              </div>
+              <div className="col-sm-6 col-md-2">
+                <select id="selectCinema" className="form-control">
+                  <option selected>Rạp</option>
+                  <option>item.TenRap</option>
+                </select>
+              </div>
+              <div className="col-sm-6 col-md-2">
+                <select id="selectDate" className="form-control">
+                  <option selected>Ngày</option>
+                  <option>item.NgayChieuGioChieu | date: "dd/MM/yyyy"</option>
+                </select>
+              </div>
+              <div className="col-sm-6 col-md-2">
+                <select id="selectTime" className="form-control">
+                  <option selected>Giờ</option>
+                  <option>item.NgayChieuGioChieu | date: "hh:mm"</option>
+                </select>
+              </div>
+              <div className="col-sm-12 col-md-2">
+                <button className="btn-booking btn">Đặt vé</button>
+              </div>
+            </div>
           </div>
-          <div>
-            <img src="/img/images/carousel/carousel2.jpg" alt="GTA V" />
-          </div>
-          <div>
-            <img src="/img/images/carousel/carousel3.jpg" alt="Mirror Edge" />
-          </div>
-        </OwlCarousel>
+        </div>
       </section>
     );
   }
