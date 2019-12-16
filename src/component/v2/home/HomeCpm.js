f37375cea6c5047aa78cdcd3b400ae6731b598a4
bennguyen232 <nguyenduythai427@gmail.com>
2019-12-16 10:52:51 +0700

    format

--- src/component/v2/home/HomeCpm.js
+++ src/component/v2/home/HomeCpm.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import './styles/home.scss'
 import GlobalCenimasCpm from './GlobalCenimasCpm'
 import CarouselCpm from './CarouselCpm'
-import SearchCpm from './SearchCpm'
 import FilmsCpm from './film/FilmsCpm'
 import NewsCpm from './news/NewsCpm'
 
@@ -11,7 +10,6 @@ export default class HomeCpm extends Component {
     return (
       <div className="cpm-home">
         <CarouselCpm/>
-        <SearchCpm/>
         <FilmsCpm/>
         <GlobalCenimasCpm/>
         <NewsCpm/>
--- src/redux/reducer/action.js
+++ src/redux/reducer/action.js
@@ -1,138 +1,154 @@
-import { getAllFilm,addMovie,updateMovie} from "../../services/movieSevice"
-import { Login, CreateUser, UpdateUser, ListUser, UserDetail, AddUser, UserType} from "../../services/userService"
-import { DETAIL_USER, UPDATE_USER, LOG_IN, UPDATE_MOVIES, ADD_MOVIES, GET_MOVIES, ADD_USER, TYPE_USER, CREATE_USER } from "../constantReducer"
+import { getAllFilm, addMovie, updateMovie } from "../../services/movieSevice";
+import {
+  Login,
+  CreateUser,
+  UpdateUser,
+  ListUser,
+  UserDetail,
+  AddUser,
+  UserType
+} from "../../services/userService";
+import {
+  DETAIL_USER,
+  UPDATE_USER,
+  LOG_IN,
+  UPDATE_MOVIES,
+  ADD_MOVIES,
+  GET_MOVIES,
+  ADD_USER,
+  TYPE_USER,
+  CREATE_USER
+} from "../constantReducer";
 
-export const reduxAction = (type,data) => {
-  return {type, data}
-}
+export const reduxAction = (type, data) => {
+  return { type, data };
+};
 
 // Quản lý mọi thứ về film
 //Lấy danh sách phim
 export const fetchAllMovies = () => {
   return dispatch => {
     getAllFilm()
-    .then(res => {
-      dispatch(reduxAction(GET_MOVIES, res.data));
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(GET_MOVIES, res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+};
 //thêm movie
 export const addMovies = () => {
   return dispatch => {
     addMovie()
-    .then(res => {
-      dispatch(reduxAction(ADD_MOVIES, res.data));
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(ADD_MOVIES, res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+};
 //update movie
 export const updateMovies = () => {
   return dispatch => {
     updateMovie()
-    .then(res => {
-      dispatch(reduxAction(UPDATE_MOVIES, res.data));
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(UPDATE_MOVIES, res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+};
 
 // End quản lý film
 /************** */
-
-
 
 // Quản lý user
 //User đăng nhập
 export const loginUser = () => {
   return dispatch => {
     Login()
-    .then(res => {
-      dispatch(reduxAction(LOG_IN, res.data));
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-}
-//tạo account 
+      .then(res => {
+        dispatch(reduxAction(LOG_IN, res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+};
+//tạo account
 export const createUser = () => {
   return dispatch => {
     CreateUser()
-    .then(res => {
-      dispatch(reduxAction(CREATE_USER, res.data));
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(CREATE_USER, res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+};
 //cập nhật user
 export const updateUser = () => {
   return dispatch => {
     UpdateUser()
-    .then(res => {
-      dispatch(reduxAction(UPDATE_USER,res.data));
-    })
-    .cath(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(UPDATE_USER, res.data));
+      })
+      .cath(err => {
+        console.log(err);
+      });
+  };
+};
 //quan ly nguoi dung user
 export const userDetail = () => {
   return dispatch => {
     UserDetail()
-    .then(res => {
-      dispatch(reduxAction(DETAIL_USER,res.data));
-    })
-    .cath(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(DETAIL_USER, res.data));
+      })
+      .cath(err => {
+        console.log(err);
+      });
+  };
+};
 //thêm người dùng
 export const addUser = () => {
   return dispatch => {
     AddUser()
-    .then(res => {
-      dispatch(reduxAction(ADD_USER,res.data));
-    })
-    .cath(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(ADD_USER, res.data));
+      })
+      .cath(err => {
+        console.log(err);
+      });
+  };
+};
 
 //danh sach loai nguoi dung
 export const typeUser = () => {
   return dispatch => {
     UserType()
-    .then(res => {
-      dispatch(reduxAction(TYPE_USER,res.data));
-    })
-    .cath(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(TYPE_USER, res.data));
+      })
+      .cath(err => {
+        console.log(err);
+      });
+  };
+};
 //danh sach nguoi dung
 export const listUser = () => {
   return dispatch => {
     ListUser()
-    .then(res => {
-      dispatch(reduxAction(TYPE_USER,res.data));
-    })
-    .cath(err => {
-      console.log(err);
-    })
-  }
-}
+      .then(res => {
+        dispatch(reduxAction(TYPE_USER, res.data));
+      })
+      .cath(err => {
+        console.log(err);
+      });
+  };
+};
 // End quản lý user *//////////
