7ea7354087f5a831cf8e72deb09b51d442d887b3
Van Nguyen <nguyenvananh9987@gmail.com>
2019-12-18 16:11:11 +0700

    feat: add history

--- src/component/v2/admin/StoryBook.js
+++ src/component/v2/admin/StoryBook.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import './style/userManagement.scss'
+import MaterialTable from 'material-table';
+import { ListHistory } from '../../../services/historyService';
 
 class StoryBook extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      columns: [
+        { title: 'Mã ghế', field: 'maGhe' },
+        { title: 'Tên ghế', field: 'tenGhe' },
+        { title: 'Loại ghế', field: 'loaiGhe' },
+        { title: 'Mã ghế', field: 'maGhe' },
+        { title: 'Mã rạp', field: 'maRap' },
+        { title: 'STT', field: 'stt' },
+        { title: 'Tài khoản người đặt', field: 'taiKhoanNguoiDat'},
+      ],
+      data: []
+    }
+  }
   render() {
     return (
-      <div>
-        
+      <div className="cm-user-management">
+        <div class="title__user">
+          <h2>Lịch sử đặt vé</h2>
+        </div>
+        <MaterialTable
+          title=""
+          columns={this.state.columns}
+          data={this.state.data}
+        />
       </div>
     );
+  }
+
+  componentDidMount() {
+    ListHistory().then(res => {
+      const { data } = res
+      let mangvedat = []
+      data.danhSachGhe.map(data=>{
+        if (data.taiKhoanNguoiDat!=null) {
+          mangvedat.push(data)
+        }
+      })
+      console.log(mangvedat);
+      
+
+      this.setState({ data:mangvedat})
+
+    })
   }
 }
 
